32599e5052c94b08b72fd1ffac4bd3328676c6f8
Loris Mancel <lm@taktik.com>
2021-12-28 16:21:59 +0100

    use generic logger typings
    removed src from publish
    do not build tests

--- src/stateMachine.ts
+++ src/stateMachine.ts
@@ -1,4 +1,5 @@
-import { IState, ListenerRegistration, Logger, StateMachine, Transitions } from './typings'
+import type { Logger } from 'generic-logger-typings'
+import { IState, ListenerRegistration, StateMachine, Transitions } from './typings'
 
 export class StateMachineImpl<T extends State> implements StateMachine<T> {
 	readonly states: T[]
